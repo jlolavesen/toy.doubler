e7d62a2890b8c79ee4483ccc2c3d2b9539cecb6c
jlolavesen <jlolavesen@gmail.com>
2023-12-09 17:04:47 -0500

    add documentation and test

--- R/toy_double.R
+++ R/toy_double.R
@@ -1,3 +1,13 @@
+#' Title
+#'
+#' @param x a numerical vector
+#'
+#' @return a numerical vector, doubled
+#' @export
+#'
+#' @examples
+#' x <- c(1,2,3)
+#' toy_double(x)
 toy_double <- function(x) {
   (2 * x)
 }
---
+++ tests/testthat/test-toy_double.R
@@ -0,0 +1,3 @@
+test_that("toy_double() doubles a vector", {
+  expect_equal(toy_double(c(1,2)), c(2,4))
+})
